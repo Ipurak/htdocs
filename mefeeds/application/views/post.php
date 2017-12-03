213b4d2e73809f156189ccee0beea0065b5ed6c2
Rungrot Suptan <asmi.rungrot@gmail.com>
2017-12-03 18:28:56 +0700

    committed

--- mefeeds/application/views/post.php
+++ mefeeds/application/views/post.php
@@ -13,11 +13,11 @@
   <link rel="stylesheet" href="<?=site_url("public/css/mefeeds.css");?>" rel="stylesheet" type="text/css">
   <link rel="stylesheet" href="<?=site_url("public/sweetalert2/dist/sweetalert2.min.css");?>" rel="stylesheet" type="text/css">
 
-  <meta property="og:url" content="<?php echo $strCurrentURL;?>" />
+  <meta property="og:url" content="<?=base_url(uri_string())?>" />
   <meta property="og:type" content="website" />
-  <meta property="og:title" content="วิธีการทำระบบ Login ด้วยฐานข้อมูล PHP กับ MySQL Database แบบ Step by Step" />
-  <meta property="og:description" content="บทความสอนเกี่ยวกับการทำระบบ login ด้วย php กับ mysql แบบง่ายๆ แบบ stey by step ตั้งแต่พื้นฐานการสร้าง table บน mysql และารเขียน code ด้วย php" />
-  <meta property="og:image" content="http://www.thaicreate.com/tutorial/php-mysql.jpg" />
+  <meta property="og:title" content="<?=$post[0]->title?>" />
+  <meta property="og:description" content="<?=$post[0]->desc?>" />
+  <meta property="og:image" content="http://bulma.io/images/placeholders/1280x960.png" />
 
   <style type="text/css">
       
@@ -69,27 +69,6 @@
             <div id="feeds">
               <div class="tile is-child box" v-for="feed in feeds">
 
-                <!--##### [START] Facebook Shere Button #####-->
-
-                  <div id="fb-root"></div>
-                  <script>(function(d, s, id) {
-
-                    var js, fjs = d.getElementsByTagName(s)[0];
-                    if (d.getElementById(id)) return;
-                    js = d.createElement(s); js.id = id;
-                    js.src = 'https://connect.facebook.net/th_TH/sdk.js#xfbml=1&version=v2.11';
-                    fjs.parentNode.insertBefore(js, fjs);
-
-                    }(document, 'script', 'facebook-jssdk'));
-
-                  </script>
-
-                  <div class="fb-share-button" data-href="https://developers.facebook.com/docs/plugins/" data-layout="button_count" data-size="small" data-mobile-iframe="true">
-                    <a class="fb-xfbml-parse-ignore" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse">แชร์</a>
-                  </div>
-
-                <!-- ##### [END] Facebook Shere Button ##### -->
-
                 <div class="card">
                   <div class="card-image">
                     <figure class="image is-4by3">
@@ -117,6 +96,51 @@
                       <small class="dateformat"><?=$post[0]->dateauto ?></small>
                     </div>
                   </div>
+
+                  <!--##### [START] Facebook Shere Button #####-->
+
+                  <div id="fb-root"></div>
+                  <script>(function(d, s, id) {
+
+                    var js, fjs = d.getElementsByTagName(s)[0];
+                    if (d.getElementById(id)) return;
+                    js = d.createElement(s); js.id = id;
+                    js.src = 'https://connect.facebook.net/th_TH/sdk.js#xfbml=1&version=v2.11';
+                    fjs.parentNode.insertBefore(js, fjs);
+
+                    }(document, 'script', 'facebook-jssdk'));
+
+                  </script>
+
+                  <div class="fb-share-button" data-href="<?=base_url(uri_string())?>" data-layout="button_count" data-size="small" data-mobile-iframe="true">
+                    <a class="fb-xfbml-parse-ignore" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse">แชร์</a>
+                  </div>
+
+                <!-- ##### [END] Facebook Shere Button ##### -->
+
+                  <div class="line-it-button" data-lang="en" data-type="share-a" data-url="<?=base_url(uri_string())?>" style="display: none;"></div>
+                  <script src="https://d.line-scdn.net/r/web/social-plugin/js/thirdparty/loader.min.js" async="async" defer="defer"></script>
+
+                <!-- ##### [START] LINE Shere Button ##### -->
+
+
+
+                <!-- ##### [END] LINE Shere Button ##### -->
+
+                <!-- ##### [START] Facebook Comment ##### -->
+                <div id="fb-root"></div>
+                <script>
+                  (function(d, s, id) {
+                    var js, fjs = d.getElementsByTagName(s)[0];
+                    if (d.getElementById(id)) return;
+                    js = d.createElement(s); js.id = id;
+                    js.src = 'https://connect.facebook.net/th_TH/sdk.js#xfbml=1&version=v2.11';
+                    fjs.parentNode.insertBefore(js, fjs);
+                  }(document, 'script', 'facebook-jssdk'));
+                </script>
+                <div class="fb-comments" data-href="https://developers.facebook.com/docs/plugins/comments#configurator" data-numposts="5"></div>
+                <!-- ##### [END] Facebook Comment ##### -->
+
                 </div>
 
               </div>
